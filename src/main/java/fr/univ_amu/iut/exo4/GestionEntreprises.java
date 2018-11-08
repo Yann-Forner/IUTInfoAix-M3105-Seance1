edf037998862a4bc4004c0e1a0cb618d45fba425
Forner Yann <37612939+Yann-Forner@users.noreply.github.com>
2018-11-08 10:32:22 +0100

    Add files via upload

---
+++ src/main/java/fr/univ_amu/iut/exo4/GestionEntreprises.java
@@ -0,0 +1,7 @@
+package fr.univ_amu.iut.exo4;
+
+public class GestionEntreprises {
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented !");
+    }
+}
---
+++ src/main/java/fr/univ_amu/iut/exo5/GestionEntreprises.java
@@ -0,0 +1,7 @@
+package fr.univ_amu.iut.exo5;
+
+public class GestionEntreprises {
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented !");
+    }
+}
