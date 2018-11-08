39b94c0cd2588d2ba2e508ec096443bd61a5b1b0
f17007588 <yann.forner@etu.univ-amu.fr>
2018-11-08 13:45:22 +0100

    debut exo3

--- src/main/java/fr/univ_amu/iut/exo3/GestionEntreprises.java
+++ src/main/java/fr/univ_amu/iut/exo3/GestionEntreprises.java
@@ -2,6 +2,23 @@ package fr.univ_amu.iut.exo3;
 
 public class GestionEntreprises {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+        Vendeur tristan = new Vendeur(
+                0,
+                "05056416541560",
+                "LAROUBINE",
+                0,
+                LocalDate.of(2000, Month.NOVEMBER, 16),
+                LocalDate.of(1999, Month.NOVEMBER, 16),10,50,1000000,0.0000001);
+        Representant teo = new Representant(
+                1,
+                "040",
+                "MARTIN",
+                0,
+                LocalDate.of(2000, Month.NOVEMBER, 16),
+                LocalDate.of(1999, Month.NOVEMBER, 16),10,50,1000000,0.0000001);
+        Entreprise yannCorp= new Entreprise("YannCorp");
+        yannCorp.embaucher(tristan);
+        yannCorp.embaucher(teo);
+        System.out.println(yannCorp.toString());
     }
 }
